05535b59b62ef889f4372c886e341b022dd1fedc
Kailie <kailie.yuan@cix.csi.cuny.edu>
2018-06-22 11:41:51 -0400

    Can check if username is valid

--- index.py
+++ index.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
-    #return "<h1>Hello There!!!</h1>"
+
+@app.route('/login/<username>')
+def login(username):
+    if username == "Kay":
+        return render_template('login.html', username=username)
+    else:
+        return render_template('errorlogin.html', username=username)
 
 if __name__ == "__main__":
     app.run()
