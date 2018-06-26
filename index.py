3aabb34c5310b6db2f379c43e22b328b118ce5ed
Kailie <kailie.yuan@cix.csi.cuny.edu>
2018-06-26 16:08:39 -0400

    login function looks similr in how it works in php

--- index.py
+++ index.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route('/login/<username>')
-def login(username):
-    if username == "Kay":
-        return render_template('login.html', username=username)
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'admin':
+            return render_template('login.html', username)
+        else:
+            return render_template('errorlogin.html', username)
     else:
-        return render_template('errorlogin.html', username=username)
+        return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run()
